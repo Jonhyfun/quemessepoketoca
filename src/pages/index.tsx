700a2f5002ddbf25877ccdc0ccd52e50b8c6ca5c
João Paulo Grimaldi <jp.grimaldi@nv99.com.br>
2024-02-22 00:48:52 -0300

    any

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,7 +5,7 @@ function getRandomInt(min: number, max: number) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-export async function getServerSideProps(ctx) {
+export async function getServerSideProps(ctx: any) {
   return {
     redirect: {
       permanent: false,
